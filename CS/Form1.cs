5faf1959dc87932c27e0c9dc6a46cc705cde64e8
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2024-11-18 15:03:15 +0400

    Update Form1.cs

--- CS/Form1.cs
+++ CS/Form1.cs
@@ -8,17 +8,14 @@ using DevExpress.XtraMap;
 
 namespace AzureRouting {
     public partial class Form1 : Form {
-
         const string azureKey = "your key";
         ObservableCollection<GeoPoint> geoPoints = new ObservableCollection<GeoPoint>();
         MapItemStorage itemData;
-
         InformationLayer routeInfoLayer;
         AzureRouteDataProvider azureRoute;
 
         public Form1() {
             InitializeComponent();
-
             mapControl.Layers.AddRange(
                 new LayerBase[] {
                     new ImageLayer() {
@@ -46,18 +43,14 @@ namespace AzureRouting {
             routeInfoLayer.ItemStyle.StrokeWidth = 2;
             routeInfoLayer.ItemStyle.Stroke = Color.DeepSkyBlue;
             routeInfoLayer.Error += RouteInfoLayer_Error;
-
             waypointsListBoxControl.DataSource = geoPoints;
-
             mapControl.Zoom(6);
             mapControl.SetCenterPoint(new GeoPoint(40.714627, -74.002863), false);
             mapControl.EnableRotation = false;
         }
-
         private void RouteInfoLayer_Error(object sender, MapErrorEventArgs e) {
             throw new System.NotImplementedException();
         }
-
         private void mapControl_Click(object sender, System.EventArgs e) {
             MouseEventArgs mouseEventArgs = (MouseEventArgs)e;
             if(mouseEventArgs.Button == MouseButtons.Right) {
@@ -68,26 +61,21 @@ namespace AzureRouting {
                 });
             }
         }
-        
         private void calculateRouteButton_Click(object sender, System.EventArgs e) {
-            if (geoPoints.Count <= 1)
-            {
+            if (geoPoints.Count <= 1) {
                 MessageBox.Show("Specify at least two Waypoints to calculate a route.");
                 return;
             }
-            azureRoute.CalculateRoute(geoPoints.Select(point => new RouteWaypoint("", point)).ToList(), new AzureRouteOptions()
-            {
+            azureRoute.CalculateRoute(geoPoints.Select(point => new RouteWaypoint("", point)).ToList(), new AzureRouteOptions() {
                 TravelMode = GetTravelMode(),
                 AvoidTypes = GetAvoidTypes()
             });
             geoPoints.Clear();
             itemData.Items.Clear();
         }
-
         AzureTravelMode GetTravelMode() {
             return (AzureTravelMode)Enum.Parse(typeof(AzureTravelMode), (string)travelModeComboBox.SelectedItem);
         }
-
         AzureRouteAvoidType GetAvoidTypes() {
             var avoidTypes = AzureRouteAvoidType.None;
             foreach (string item in avoidTypesCheckedListBox.CheckedItems) {
@@ -96,6 +84,5 @@ namespace AzureRouting {
             }
             return avoidTypes;
         }
-
     }
 }
